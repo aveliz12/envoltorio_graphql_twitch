63bd3984411c98d4dcac48e970adb4f16f1b74c5
aveliz12 <alexpaulveliz12@gmail.com>
2022-12-11 15:41:36 -0500

    Uso de variables de entorno

--- cnn/resolvers.js
+++ cnn/resolvers.js
@@ -1,19 +1,17 @@
 const fetch = require("node-fetch");
-require("dotenv").config({ path: "../variables.env" });
+require("dotenv").config();
 const Storage = require("node-storage");
 const store = new Storage("./store");
 
-const getJsonTokenData = async (path,params) => {
+const getJsonTokenData = async (path, params) => {
   const token = store.get("token");
-  console.log(token);
-  console.log("Bearer " + token)
   const response = await fetch(`https://api.twitch.tv/helix/${path}`, {
     method: "GET",
     headers: {
-      "Authorization": "Bearer " + token,
+      Authorization: "Bearer " + token,
       "Client-Id": "a2bf4j1rhkytvzoc4ortzn7m4yxg33",
     },
-    body:params
+    body: params,
   });
   return await response.json();
 };
@@ -22,9 +20,9 @@ const resolvers = {
   Query: {
     getToken: async () => {
       const params = new URLSearchParams();
-      params.append("client_id", "a2bf4j1rhkytvzoc4ortzn7m4yxg33");
-      params.append("client_secret", "ri98723b3c639ub3zgltykc2znqicu");
-      params.append("grant_type", "client_credentials");
+      params.append("client_id", process.env.CLIENTID);
+      params.append("client_secret", process.env.CLIENTSECRET);
+      params.append("grant_type", process.env.GRANTTYPE);
 
       try {
         const response = await fetch("https://id.twitch.tv/oauth2/token", {
@@ -32,16 +30,20 @@ const resolvers = {
           body: params,
         });
         const data = await response.json();
+        store.put("token", data.access_token);
 
         console.log(data);
-        store.put("token", data.access_token);
       } catch (error) {
         console.log(error);
       }
     },
     getLiveStreams: async () => {
-      const response = await getJsonTokenData("streams");
-      console.log(response);
+      try {
+        const response = await getJsonTokenData("streams");
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
